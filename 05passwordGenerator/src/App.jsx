c5cc68b208d701a32318c520410615f3f5e7e1c6
karanbgit <karanbarale7047@gmail.com>
2025-02-01 23:14:33 +0530

    `Refactor password generator component to include user state and password generation logic`

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,16 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // All UserState objects Created by this Component 
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPasssword] = useState("");
+
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()_"
+
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+
+      pass = str.charAt(char);
+
+
+    }
+
+    setPasssword(pass);
+
+
+
+  }, [length, numberAllowed, charAllowed, setPasssword]);
+
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+
+      <div className="w-full max-w-md mx-auto shawod-md rounded-lg px-4 my-10 text-orange-500 bg-gray-700">
+        <h1 className="text-4xl text-center text-white">Password Generator</h1>
+
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
+          <input type="text" value={password} className="outline-none bg-white w-full py-1 px-3"
+            placeholder='password' readOnly />
+        </div>
+
+      </div>
+
     </>
-  )
+  ) 
+
+
 }
 
 export default App
