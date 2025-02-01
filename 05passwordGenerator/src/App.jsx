4df640a201f4806dd4eeca144646f276078e7c54
karanbgit <karanbarale7047@gmail.com>
2025-02-02 00:37:25 +0530

    `Refactor password generator UI and add copy button and input range bar`

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -37,19 +37,60 @@ function App() {
   return (
     <>
 
-      <div className="w-full max-w-md mx-auto shawod-md rounded-lg px-4 my-10 text-orange-500 bg-gray-700">
-        <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shawod-md rounded-lg px-4 py-3 my-10  bg-gray-800 text-orange-500">
+        <h1 className="text-2xl text-center text-white my-4">Password Generator</h1>
 
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
 
-          <input type="text" value={password} className="outline-none bg-white w-full py-1 px-3"
-            placeholder='password' readOnly />
+          <input type="text" value={password}
+            className="outline-none bg-white w-full py-1 px-3"
+            placeholder='Password' readOnly />
+
+          <button className="outline-none bg-blue-700 text-white px-3
+            py-0.5 shrink-0"> Copy</button>
         </div>
+
+
+        <div className="flex text-sm gap-x-2">
+          {/* start  input range bar */}
+          <div className="flex items-center gap-x-1">
+            <input type="range"
+              min={6} max={100} value={length} className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }} />
+            <label>Length: {length}</label>
+          </div>
+          {/* end input range bar */}
+
+          {/* Number Checkbox start */}
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox" defaultChecked={numberAllowed} id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          {/* Number Checkbox end */}
+
+          {/* Character Checkbox start */}
+          <div className="flex items-center gap-x-1">
+            <input type="checkbox" defaultChecked={charAllowed} id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+          {/* Character Checkbox end */}
+        </div>
+
+
+
 
       </div>
 
     </>
-  ) 
+  )
 
 
 }
