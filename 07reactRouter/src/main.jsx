6419877a777cf492f053eb9b47876ffe6bc6db58
karanbgit <karanbarale7047@gmail.com>
2025-02-15 01:54:43 +0530

    `feat: updated footer and added github link in header and routing for github page`

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import User from './components/User/User.jsx';
+import Github, { githubInfoLoader } from './components/Github/Github.jsx';
 
 // First Method to Routing
 
@@ -42,6 +43,11 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github'
+        element={<Github />}
+      />
     </Route>
   )
 )
